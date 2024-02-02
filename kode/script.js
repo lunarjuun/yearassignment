3e4a0fe67cedc441691c9e44d7234897df851259
Dominic Aramis Aguilar <elfia007@osloskolen.no>
2024-02-02 10:10:40 +0100

    adding my work

---
+++ kode/script.js
@@ -0,0 +1,8 @@
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementsByClassName("title")[0].innerHTML = result;
+
+  console.log(document) 
